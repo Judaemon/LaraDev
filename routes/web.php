d821d9e87fad2a9a5787ed964598fd2276952264
John ray Judaya <jrj@newmediastaff.com>
2025-03-12 17:48:31 +0800

    chore: add .dockerignore, update Dockerfile for better caching, and implement user and config routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,44 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// simple list of user josn route
+Route::get('/users', function () {
+    $users = User::all();
+    return response()->json($users);
+});
+
+
+// simple route for config
+Route::get('/config', function () {
+    // phpinfo();
+
+    $scriptPath = base_path('database/setup-db.sh');
+
+    // Ensure the script is executable
+    if (!is_executable($scriptPath)) {
+        chmod($scriptPath, 0755);
+    }
+
+    // Run the script using /bin/sh
+    $process = new Process(['/bin/sh', $scriptPath]);
+    $process->setWorkingDirectory(base_path()); // Ensure it runs from Laravel's root
+    $process->run();
+
+    // Check for errors
+    if (!$process->isSuccessful()) {
+        throw new ProcessFailedException($process);
+    }
+
+    return response()->json([
+        'message' => 'Database setup completed successfully.',
+        'output' => $process->getOutput(),
+    ]);
 });
